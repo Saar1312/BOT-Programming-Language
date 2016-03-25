f14ddd7cfbf2edf143b6ae0048972e9d5a4475fe
SergioTeranZ <sergioteran.zambrano@gmail.com>
2016-03-25 19:18:41 -0430

    Ahora los errores son llamados desde el archivo errores.py

--- Tabla.py
+++ Tabla.py
@@ -6,7 +6,7 @@ import sys
 from Parser import *
 from Pila import *
 from Arbol import *
-
+from errores import *
 class Tabla:
 	def __init__(self,exterior):
 		self.tablaExterna = exterior
@@ -74,6 +74,7 @@ class Tabla:
 	#
 	# Permite saber si un objeto es un arbol
 	#--------------------------------------------------------------------------	
+
 def esArbol(h):
 	if type(h) in [arbol,expresion,instContr,\
 				instRobot]:
@@ -94,23 +95,33 @@ comp = None # Nodo raiz de los comportamientos de un robot
 	# Crea la tabla de simbolos, realiza la verificacion de tipos e imprime 
 	# errores semanticos (hace toda vaina)
 	#--------------------------------------------------------------------------	
+
+def cambiar_tope(p,pointer):
+	tope = p.getTope()
+	p.addTope(pointer)
+	pointer = tope
+	return pointer
+
 def crearTabla(arbol,almacenar):
 	global tipo,pointer,p,incAlcance,bots,comp
-	if arbol.nombre == 'INSTRUCCIONES_ROBOT': # Crea la tabla para la inc de alcance solo si hay una inc de 
+	
+	if arbol.nombre in  ['INSTRUCCIONES_ROBOT','INICIO']: # Crea la tabla para la inc de alcance solo si hay una inc de 
 		almacenar = True					  # alcance y en esta se declaran robots
-	elif arbol.nombre == 'INICIO':
-		almacenar = True
+	
 	elif arbol.nombre == 'EXECUTE':
 		almacenar = False
+	
 	elif arbol.nombre == 'CONDICION': # Arbol de las instrucciones On activation/default/...
 		p.addTope(pointer)
 		t = list(pointer.tabla.values())[0].tabla # Mueve el apuntador a la tabla de simbolos asociada a cualquiera de las variables
 		pointer = t
+	
 	elif arbol.nombre == 'INC_ALCANCE': # NUEVO Para bajar el apuntador de tablas a un nuevo nivel inferior
 		p.addTope(pointer)
 		t = Tabla(pointer)
 		pointer = t
 		arbol.tabla = t
+	
 	if esArbol(arbol):
 		if almacenar:
 			if arbol.nombre == 'DECLARACION_ROBOT':
@@ -136,9 +147,7 @@ def crearTabla(arbol,almacenar):
 			elif (arbol.nombre == 'COLLECT' and 
 				 arbol.hijos != []): 
 				if arbol.hijos[0].nombre == 'COLLECT_AS':
-					tope = p.getTope()
-					p.addTope(pointer)
-					pointer = tope
+					pointer = cambiar_tope(p,pointer)
 					simbolo = arbol.hijos[0].hijos[0].hijos[0]
 					arbol.hijos[0].hijos[0].tipo = tipo
 					for robot,datos in pointer.tabla.items():
@@ -147,9 +156,7 @@ def crearTabla(arbol,almacenar):
 
 			elif (arbol.nombre == 'READ_AS' and
 				 arbol.hijos != []):
-				tope = p.getTope()
-				p.addTope(pointer)
-				pointer = tope
+				pointer = cambiar_tope(p,pointer)			
 				simbolo = arbol.hijos[0].hijos[0]
 				arbol.hijos[0].tipo = tipo
 				for robot,datos in pointer.tabla.items():
@@ -157,88 +164,63 @@ def crearTabla(arbol,almacenar):
 				pointer = p.popTope()
 
 			elif arbol.nombre == 'ACTIVACION':
-				tope = p.getTope()
-				p.addTope(pointer)
-				pointer = tope
+				pointer = cambiar_tope(p,pointer)				
 				for robot in bots:
 					datos = pointer.tabla[robot]
 					if datos.estado == 'activacion':
-						print("Error en la linea %d:" % (arbol.linea))
-						print("Un robot no puede tener dos comportamientos de activacion.")
-						sys.exit()
+						error_linea(1,arbol)
+
 					elif datos.estado == None:
 						datos.estado = 'activacion'
 					elif datos.estado == 'desactivacion':
-						print("Error en la linea %d:" % (arbol.linea))
-						print("Un comportamiento \"activation\" no puede estar precedido por \
-														un \"deactivation\" .")
-						sys.exit()
+						error_linea(2,arbol)
+
 					elif datos.tieneDefault:
-						print("Error en la linea %d:" % (arbol.linea))
-						print("Un comportamiento \"activation\" no puede estar precedido por \
-														un \"default\" .")
-						sys.exit()
+						error_linea(3,arbol)
 				pointer = p.popTope()
 
 			elif arbol.nombre == 'DESACTIVACION':
-				tope = p.getTope()
-				p.addTope(pointer)
-				pointer = tope
+				pointer = cambiar_tope(p,pointer)				
 				for robot in bots:
 					datos = pointer.tabla[robot]
 					if datos.estado == 'activacion':
 						datos.estado = 'desactivacion'
 					elif datos.estado == None:
-						print("Error en la linea %d:" % (arbol.linea))
-						print("El comportamiento \"deactivation\" debe estar precedido por\
-																un activacion.")
-						sys.exit()
+						error_linea(4,arbol)
+
 					elif datos.estado == 'desactivacion':
-						print("Error en la linea %d:" % (arbol.linea))
-						print("Un robot no puede tener dos comportamientos de desactivacion.")
-						sys.exit()
+						error_linea(5,arbol)
 				pointer = p.popTope()
 
 			elif arbol.nombre == 'DEFAULT':
-				tope = p.getTope()
-				p.addTope(pointer)
-				pointer = tope
+				pointer = cambiar_tope(p,pointer)				
 				for robot in bots:
 					datos = pointer.tabla[robot]
 					if not datos.tieneDefault:
 						datos.tieneDefault = True
 					elif datos.tieneDefault:
-						print("Error en la linea %d:" % (arbol.linea))
-						print("Un robot no puede tener dos comportamientos default.")
-						sys.exit()
+						error_linea(6,arbol)
+
 					if datos.estado == None:
-						print("Error en la linea %d:" % (arbol.linea))
-						print("El comportamiento debe estar precedido por un \"activation\".")
-						sys.exit()
+						error_linea(7,arbol)
+
 					elif datos.estado == 'desactivacion':
-						print("Error en la linea %d:" % (arbol.linea))
-						print("El comportamiento no puede estar precedido por un \"deactivation\".")
-						sys.exit()
+						error_linea(8,arbol)
+
 				pointer = p.popTope()
 
 			elif arbol.nombre == 'ON_EXPRESION':
-				tope = p.getTope()
-				p.addTope(pointer)
-				pointer = tope
+				pointer = cambiar_tope(p,pointer)				
 				for robot in bots:
 					datos = pointer.tabla[robot]
 					if datos.estado == None:
-						print("Error en la linea %d:" % (arbol.linea))
-						print("El comportamiento debe estar precedido por un \"activation\".")
-						sys.exit()
+						error_linea(9,arbol)
 					elif datos.estado == 'desactivacion':
-						print("Error en la linea %d:" % (arbol.linea))
-						print("El comportamiento no puede estar precedido por un \"deactivation\".")
-						sys.exit()
+						error_linea(10,arbol)
+
 					if datos.tieneDefault:
-						print("Error en la linea %d:" % (arbol.linea))
-						print("El comportamiento no puede estar precedido por un \"default\".")
-						sys.exit()
+						error_linea(11,arbol)
+
 				pointer = p.popTope()
 
 		for rama in arbol.hijos:
@@ -251,40 +233,21 @@ def crearTabla(arbol,almacenar):
 						if rama.nombre in ['CONJUNCION','DISYUNCION']:
 							if rama.hijos[0].tipo == rama.hijos[1].tipo:
 								if rama.hijos[0].tipo != 'bool':
-									print("Error de tipos en la linea %d:" % (rama.linea))
-									print("No es posible operar elementos del tipo \"%s\""%\
-															  (rama.hijos[0].tipo),end=" ")
-									print("con un operador booleano.")
-									sys.exit()
+									error_tipo(1,rama)
+
 							else:
-								print("Error de tipos en la linea %d:" % (rama.linea))
-								print("No es posible operar un elemento del tipo \"%s\""%\
-														  (rama.hijos[0].tipo),end=" ")
-								print("con otro de tipo \"%s\"."% (rama.hijos[1].tipo))
-								sys.exit()
+								error_tipo(2,rama)
 
 						elif rama.nombre in ['DISTINTO','IGUALDAD']:
 							if rama.hijos[0].tipo != rama.hijos[1].tipo:
-								print("Error de tipos en la linea %d:" % (rama.linea))
-								print("No es posible operar un elemento del tipo \"%s\""%\
-														  (rama.hijos[0].tipo),end=" ")
-								print("con otro de tipo \"%s\"."%(rama.hijos[1].tipo))
-								sys.exit()
+								error_tipo(2,rama)
 
 						elif rama.nombre in ['MENOR_QUE','MENOR_IGUAL','MAYOR','MAYOR_IGUAL']:
 							if rama.hijos[0].tipo == rama.hijos[1].tipo:
 								if rama.hijos[0].tipo != 'int':
-									print("Error de tipos en la linea %d:" % (rama.linea))
-									print("No es posible operar elementos del tipo \"%s\""%\
-															  (rama.hijos[0].tipo),end=" ")
-									print("con un operador booleano.")
-									sys.exit()
+									error_tipo(1,rama)
 							else:
-								print("Error de tipos en la linea %d:" % (rama.linea))
-								print("No es posible operar un elemento del tipo \"%s\""%\
-														  (rama.hijos[0].tipo),end=" ")
-								print("con otro de tipo \"%s\"."% (rama.hijos[1].tipo))
-								sys.exit()
+								error_tipo(3,rama)
 
 						elif rama.nombre in ['SUMA','RESTA','MULTIPLICACION','DIVISION','MODULO']:
 							if rama.hijos[0].tipo == rama.hijos[1].tipo:
